444ce39efbe7614a63018d05018a5f95d5bac5ed
JulianNunzLopz <dev.nunezlopez.julian@gmail.com>
2015-10-30 20:25:03 +0100

    mise en page du code

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,6 @@
 $(function() {
 	jQuery.scrollSpeed(100, 800);
 
-	/** Initialisation 
-	$('#user_name').val('Nom'); 
-	$('#user_email').val('Votre@email'); 
-	$('#user_message').val('Tapez votre message'); **/
-
-	/** Animations **/
 	$('.niceScroll').click(function(){
 		var target = $(this).attr('href');
 		var speed = 800;
@@ -17,24 +11,4 @@ $(function() {
 	$('#menu').click(function(){
 		$('#mobilMenu nav').toggle('slow');
 	})
-
-	/*Formulaire
-	$('#user_name').click(function(){
-		if($('#user_name').val() == 'Nom')
-		{
-			$('#user_name').val('');
-		}
-	});
-	$('#user_email').click(function(){
-		if($('#user_email').val() == 'Votre@email')
-		{
-			$('#user_email').val('');
-		}
-	});
-	$('#user_message').click(function(){
-		if($('#user_message').val() == 'Tapez votre message')
-		{
-			$('#user_message').val('');
-		}
-	});*/
 });
